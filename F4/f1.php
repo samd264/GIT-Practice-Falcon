b1bc2baccadd34167a98bacc9d90a4271bd71875
Sameer <desaisameer264@yahoo.com>
2020-05-17 19:27:28 +0530

    Committing only index.php file and and newly created folder F4

---
+++ F4/f1.php
@@ -0,0 +1,3 @@
+<?
+
+echo "Created new file under branch cool_new_feature with name f1 in new folder F4";
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?
 
 echo "I have done with this in master branch!!!";
+
+echo "Modification inside F1 folder:
+	f1 - updated,
+	f2 - file renamed with f2_rename,
+	f3 - file deleted,
+	f4_new - added new file";
+
+echo "New f1 file in F4 Folder";
